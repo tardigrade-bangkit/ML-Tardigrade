428847726f72672fa234afd7583adfdae93dca97
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-06-13 07:32:18 +0700

    tidyup code

--- modul_update_ocr/main.py
+++ modul_update_ocr/main.py
@@ -1,10 +1,6 @@
-import cv2
-import ocr
 # loads the input image
-image_path = 'atam.jpg'
+image_path = 'apel.jpg'
+daun = image_path
 image = cv2.imread(image_path)
-#from google.colab import files
-#image = files.upload() 
-from google.colab.patches import cv2_imshow
-cv2_imshow(image)
+tinggi, lebar = image.shape[:2]
 prediksi(image)
