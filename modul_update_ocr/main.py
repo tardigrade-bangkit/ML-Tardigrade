893d1001b4890c494ad54d86927c1fe518a5689d
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-06-13 07:37:32 +0700

    Update main.py

--- modul_update_ocr/main.py
+++ modul_update_ocr/main.py
@@ -1,6 +1,7 @@
+import ocr
 # loads the input image
 image_path = 'apel.jpg'
 daun = image_path
 image = cv2.imread(image_path)
 tinggi, lebar = image.shape[:2]
-prediksi(image)
+ocr.prediksi(image)
